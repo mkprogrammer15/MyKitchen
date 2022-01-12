31ab55f105a9f1477f25d1b5669ccb67c0ba3aac
Michael Karp <mkprogrammer15@gmail.com>
2022-01-12 09:55:00 +0100

    refactoring of app imports

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,29 +1,5 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:profi_neon/src/app_localizations.dart';
-import 'package:profi_neon/src/core/presentation/blocs/firebase_user_auth_bloc.dart';
-import 'package:profi_neon/src/core/presentation/blocs/first_screen_ui_bloc.dart';
-import 'package:profi_neon/src/core/presentation/blocs/language_bloc.dart';
-import 'package:profi_neon/src/core/presentation/pages/language_screen.dart';
-import 'package:profi_neon/src/core/style/theme_data.dart';
-import 'package:profi_neon/src/features/admin_auth/domain/repositories/auth_repository.dart';
-import 'package:profi_neon/src/features/admin_auth/presentation/blocs/bloc/firebase_auth_bloc.dart';
-import 'package:profi_neon/src/features/admin_auth/presentation/pages/admin_after_registration_screen.dart';
-import 'package:profi_neon/src/features/admin_auth/presentation/pages/admin_login_screen.dart';
-import 'package:profi_neon/src/features/admin_chat/presentation/blocs/bloc/admin_chat_bloc.dart';
-import 'package:profi_neon/src/features/admin_chat/presentation/pages/admin_chat_screen.dart';
-import 'package:profi_neon/src/features/admin_orders/data/repositories/request_repository_impl.dart';
-import 'package:profi_neon/src/features/admin_orders/domain/repositories/request_repository.dart';
-import 'package:profi_neon/src/features/admin_orders/presentation/blocs/bloc/requests_bloc.dart';
-import 'package:profi_neon/src/features/admin_orders/presentation/pages/admin_account_screen.dart';
-import 'package:profi_neon/src/features/admin_orders/presentation/pages/document_details_screen.dart';
-import 'package:profi_neon/src/features/user_after_request/presentation/after_request_screen.dart';
-import 'package:profi_neon/src/features/user_calculation/presentation/blocs/bloc/firebasecounter_bloc.dart';
-import 'package:profi_neon/src/features/user_calculation/presentation/blocs/bloc/kitchen_bloc.dart';
-import 'package:profi_neon/src/features/user_calculation/presentation/pages/client_counter_screen.dart';
-import 'package:profi_neon/src/features/user_sumup_form/presentation/pages/client_form_screen.dart';
-import 'package:profi_neon/src/injection.dart';
+// ignore: always_use_package_imports
+import 'export.dart';
 
 class App extends StatefulWidget {
   @override
